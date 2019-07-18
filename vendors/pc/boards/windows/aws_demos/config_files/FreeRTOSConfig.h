21a5b8bf125c0486f15c4803a05139580c3734f0
Sarena Meas <sarem@amazon.com>
2019-07-18 13:54:21 -0700

    Configurations for pen testing

--- vendors/pc/boards/windows/aws_demos/config_files/FreeRTOSConfig.h
+++ vendors/pc/boards/windows/aws_demos/config_files/FreeRTOSConfig.h
@@ -178,12 +178,12 @@ extern void vLoggingPrintf( const char * pcFormat,
  * to and from a real network connection on the host PC.  See the
  * configNETWORK_INTERFACE_TO_USE definition above for information on how to
  * configure the real network connection to use. */
-#define configMAC_ADDR0                      0x00
-#define configMAC_ADDR1                      0x11
-#define configMAC_ADDR2                      0x22
-#define configMAC_ADDR3                      0x33
-#define configMAC_ADDR4                      0x44
-#define configMAC_ADDR5                      0x21
+#define configMAC_ADDR0                      0x34
+#define configMAC_ADDR1                      0x12
+#define configMAC_ADDR2                      0x12
+#define configMAC_ADDR3                      0x22
+#define configMAC_ADDR4                      0x77
+#define configMAC_ADDR5                      0x33
 
 /* Default IP address configuration.  Used in ipconfigUSE_DHCP is set to 0, or
  * ipconfigUSE_DHCP is set to 1 but a DNS server cannot be contacted. */
